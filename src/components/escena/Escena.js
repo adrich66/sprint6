71b72b28be9c42b4728d6d0f70849929ed3baf0f
Adrià Moya Cangueiro <adri@MacBook-Air-de-Adria.local>
2022-07-25 11:44:18 +0200

    Exercise 1

--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,1 +1,7 @@
-"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+import React from "react"
+
+function PrintPhrase(){
+    
+    return <h1>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</h1>
+}
+export default PrintPhrase;
